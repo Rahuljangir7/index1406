c95f8ae2ba73f45d88fef43f9df3f82c51a75af4
Rahul <dbrpjangir9977@gmail.com>
2024-07-06 18:45:10 +0530

    Update all file

---
+++ bootstrapfolio/portfolio/script0607.js
@@ -0,0 +1,11 @@
+// script type.js  for h1 tag
+document.addEventListener("DOMContentLoaded",function(){
+    var typed = new Typed("#typed",{
+        strings:["Rahul Jangir", "Developer", "Freelancer", "Designer"],
+        typeSpeed:100,
+        backSpeed:50,
+        backDelay:2000,
+        loop:true
+    });
+});
+
