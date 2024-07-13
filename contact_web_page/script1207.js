d01598a234a8ff37c03c5148bb7dc3a1ad3a1624
Rahul <dbrpjangir9977@gmail.com>
2024-07-13 23:32:02 +0530

    update all

--- contact_web_page/script1207.js
+++ contact_web_page/script1207.js
@@ -2,11 +2,12 @@
 function validform() {
     const Form = document.getElementById("form");
     // console.log(Form);
+    let isValid = true;
     
     for(let x of Form){
         if(x.value !== ""){
+            window.location.href="./contact.html"
         }
-        console.log(x.value);
     }
 
     return false;
