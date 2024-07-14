84d69bd5667ad62bde6397e851dc7e71e57a01a4
Rahul <dbrpjangir9977@gmail.com>
2024-07-14 15:40:40 +0530

    update all files

--- contact_web_page/script1207.js
+++ contact_web_page/script1207.js
@@ -2,13 +2,18 @@
 function validform() {
     const Form = document.getElementById("form");
     // console.log(Form);
-    let isValid = true;
-    
-    for(let x of Form){
-        if(x.value !== ""){
-            window.location.href="./contact.html"
+
+    for (let count = 0; count < Form.length - 1; count++) {
+        if (Form[count].value === "") {
+            Form[count].style.borderColor = "red";
+        } else {
+            Form[count].style.borderColor = "rgba(178, 178, 178, 0.378)";
+             
+            if(count === Form.length-2){
+                window.location.href = "./houseSale0207/index.html";
+            }
         }
     }
-
+    
     return false;
 }
