69d11c1b7d0523369878cd7922dc9cdbf4d66b9b
Rahul <dbrpjangir9977@gmail.com>
2024-07-18 22:05:57 +0530

    Updated all files

---
+++ HospitalFolder/script0707.js
@@ -0,0 +1,4 @@
+"use strict"
+
+// popovers button 
+
---
+++ HospitalFolder/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./{index1406.html,script0707.js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
