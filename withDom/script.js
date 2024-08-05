50a3b6f246ed81c801702eca4c834df3d8733e67
Rahul <dbrpjangir9977@gmail.com>
2024-08-05 20:00:03 +0530

    update all files

--- withDom/script.js
+++ withDom/script.js
@@ -1,31 +1,27 @@
 function theme() {
-    let change = document.body.style;
-    let btnCh = document.getElementById('btn').style;
-    let navLine = document.getElementById("btm-line").style;
-    if (change.background == "") {
-        change.background = "#000";
-        change.color = "cyan";
-        btnCh.color = "cyan";
-        btnCh.borderColor = "cyan"
-        navLine.borderColor = "gray";
-    } else {
-        change.background = "";
-        change.color = "";
-        btnCh.color = "";
-        btnCh.borderColor = "";
-        navLine.borderColor = "";
-    }
-
+  let change = document.body.style;
+  let btnCh = document.getElementById("btn").style;
+  let navLine = document.getElementById("btm-line").style;
+  if (change.background == "") {
+    change.background = "#000";
+    change.color = "white";
+    btnCh.color = "white";
+    btnCh.borderColor = "white";
+    navLine.borderColor = "gray";
+  } else {
+    change.background = "";
+    change.color = "";
+    btnCh.color = "";
+    btnCh.borderColor = "";
+    navLine.borderColor = "";
+  }
 }
 
 let sideBar = document.getElementById("side-bar").style;
 function menuOpen() {
-    sideBar.right = "0";
+  sideBar.right = "0";
 }
 
 function menuClose() {
-    sideBar.right = "-200px";
+  sideBar.right = "-200px";
 }
-
-
-addEventListener
