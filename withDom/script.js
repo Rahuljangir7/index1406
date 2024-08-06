de0c75c42a60decdea2a7b7c75c8967649b639c8
Rahul <dbrpjangir9977@gmail.com>
2024-08-06 22:43:58 +0530

    update all files

--- withDom/script.js
+++ withDom/script.js
@@ -2,18 +2,19 @@ function theme() {
   let change = document.body.style;
   let btnCh = document.getElementById("btn").style;
   let navLine = document.getElementById("btm-line").style;
+  let anchor = document.querySelectorAll("a");
   if (change.background == "") {
     change.background = "#000";
     change.color = "white";
-    btnCh.color = "white";
     btnCh.borderColor = "white";
     navLine.borderColor = "gray";
+    anchor.forEach((val)=>val.style.color ='white');
   } else {
     change.background = "";
     change.color = "";
-    btnCh.color = "";
     btnCh.borderColor = "";
     navLine.borderColor = "";
+    anchor.forEach((val)=>val.style.color ='');
   }
 }
 
