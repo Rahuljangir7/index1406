306853bf65c5332d261dce184bf628caff9fbe07
Rahul <dbrpjangir9977@gmail.com>
2024-08-13 22:28:40 +0530

    update files

--- saadaaWebPage/saadaa.js
+++ saadaaWebPage/saadaa.js
@@ -1,31 +1,36 @@
-let clickCount = 1;
 let input = document.createElement("input");
+let isOpen = false;  // A flag to track the open/close state 
 
 function searchBar() {
     const magnifyingGlass = document.querySelector(".fa-magnifying-glass");
-    magnifyingGlass.style.position = "relative";
-    magnifyingGlass.style.zIndex = 999;
+   
+    // Set up the input field only once
+    if(!input.parentElement){
+      input.type = "text";
+      input.placeholder = "search";
+      input.classList.add("search");
+      magnifyingGlass.appendChild(input);
+      input.style.transition = "width 1s linear, opacity 1s linear";
+
+      input.addEventListener("click", (event) => {
+        event.stopPropagation();  // Stop the click event from bubbling up
+    });
+    }
     
-    input.type = "text";
-    input.placeholder = "search";
-    input.classList.add("search");
-    
-    if (clickCount == 1) {
+    if (!isOpen) {
         setTimeout(() => {
       input.style.width = "170px";
       input.style.opacity = "1";
-      input.style.transition = "1s linear";
-    }, 500);
-    
+    }, 100);
+    isOpen = true;
     input.style.zIndex = "-1";
-    clickCount++;
   } else {
     setTimeout(() => {
       input.style.width = "0";
       input.style.opacity = "0";
-      input.style.transition = "1s linear";
-    }, 500);
-    clickCount--;
+      input.value = '';
+      input.placeholder
+    }, 100);
+    isOpen = false;
   }
-  magnifyingGlass.appendChild(input);
 }
